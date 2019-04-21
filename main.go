b237192ee7a30561427427812b13a7c8c74837ed
Vinny Sabatini <vincent.sabatini@gmail.com>
2019-04-21 22:06:20 +0000

    allow setting port by env variable

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(GetServePort(), nil)
+}
+
+// GetServePort returns environment variable SERVE_PORT if set otherwise, 8080 is returned
+func GetServePort() string {
+	//TODO: do this as a command line option instead
+	port := os.Getenv("SERVE_PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
